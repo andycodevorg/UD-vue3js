81b7230124f6aaeb2d755532534b59c4a89d1a6a
andyvega <andyvega@upeu.edu.pe>
2023-02-09 17:02:53 -0500

    subiendo proyecto

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Mi componente princiapal</h1>
+        <Car :power="power" :upPower="upPower" @downPower="downPower()"/>
+    </div>
+</template>
+
+<script>
+
+import { ref } from "vue";
+import Car from "./components/Car.vue";
+
+    export default {
+        components: {
+            Car
+        },
+
+        setup() {
+            let power = ref(1000);
+
+            const upPower = () => {
+                power.value++;
+                console.log("Aumentar power desde App ...");
+            }
+
+            const downPower = () => {
+                power.value--;
+            }
+            
+
+            return {
+                power,
+                upPower,
+                downPower
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Car.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <h1>Car</h1>
+        <h3>Características</h3>
+        <ul>
+            <li>Marca: {{ brand }}</li>
+            <li>Modelo: {{ model }}</li>
+        </ul>
+        <h3>Potencia</h3>
+        <h2>{{ power }}</h2>
+
+        <!-- <button @click="testPower()">Aumentar Potencia</button> -->
+        <button @click="upPower()">Aumentar Potencia</button>
+        <button @click="downPowerFn()">Disminuir Potencia</button>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        power: {
+            type: Number,
+            default: 500
+        },
+        upPower: Function,
+    },
+
+    emits: ["downPower"],
+
+    setup(props, context) {
+        const brand = "Kia";
+        const model = "Elantra";
+
+        // console.log(context);
+        // console.log(props);
+
+        const downPowerFn = () => {
+            context.emit("downPower");
+        }
+
+        // const testPower = () => {
+        //     props.upPower();
+        // }
+     
+        return {
+            brand,
+            model,
+            downPowerFn
+            // testPower
+        }
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
